fd660ae6166164cd565374b2038942af1cf3aeaa
DELL <DELL@MYACCESS>
2024-07-05 08:32:10 +0530

    My First Commit

---
+++ src/myapp/Hello.java
@@ -0,0 +1,18 @@
+package myapp;
+
+public class Hello {
+ public static void main(String args[]) {
+ 
+	 System.out.println("Jai Sri Ganesh");
+	 
+	 int age=18;
+	 if(age>=18)
+	 {
+		 System.out.println("you are eligible to cast your vote");
+	 }
+	 else
+	 {
+		 System.out.println("you cannot cast vote");
+	 }
+ }
+}
